0be7e33a021ab1df33cd1f21442878527ce3a8de
Ravex2d0 <djmoonzhx72@gmail.com>
2026-03-12 22:17:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -13,6 +13,11 @@ const projects = [
     title: "Image Compressor & Resizer",
     desc: "Tool for resizing images and adjusting compression quality while preserving visual clarity.",
     link: "https://ravex2d0.github.io/image-tool/"
+  },
+  {
+    title: "Password Generator",
+    desc: "Generate secure passwords from plaintext using a custom key, including uppercase, lowercase, numbers, and symbols.",
+    link: "https://ravex2d0.github.io/password-generator/"
   }
 ];
 
